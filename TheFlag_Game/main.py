b3d4c29747ec18c30acf167b3c0694e256ca1ecf
dafnat19 <dafnat19@gmail.com>
2026-09-09 11:15:25 +0300

    main

--- TheFlag_Game/main.py
+++ TheFlag_Game/main.py
@@ -1,11 +1,48 @@
 import screen
 import pygame
+import soldier
+import game_field
 
-done = False
+def main():
 
-screen.draw_game()
-while not done:
+    done = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    screen.draw_game()
+    current_solider_location = (0, 0)
+    screen.draw_soldier(current_solider_location)
+    while not done:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    solider_new_location = (current_solider_location[0] , current_solider_location[1]-1)
+                    if soldier.soldier_check_location(solider_new_location):
+                        current_solider_location = solider_new_location
+                        screen.draw_soldier(current_solider_location)
+
+                elif event.key == pygame.K_DOWN:
+                    solider_new_location = (current_solider_location[0], current_solider_location[1]+1)
+                    if soldier.soldier_check_location(solider_new_location):
+                        current_solider_location = solider_new_location
+                        screen.draw_soldier(current_solider_location)
+                elif event.key == pygame.K_LEFT:
+                    solider_new_location = (current_solider_location[0]-1, current_solider_location[1] )
+                    if soldier.soldier_check_location(solider_new_location):
+                        current_solider_location = solider_new_location
+                        screen.draw_soldier(current_solider_location)
+                elif event.key == pygame.K_RIGHT:
+                    solider_new_location = (current_solider_location[0]+1, current_solider_location[1] )
+                    if soldier.soldier_check_location(solider_new_location):
+                        current_solider_location = solider_new_location
+                        screen.draw_soldier(current_solider_location)
+                if soldier.soldier_check_mine(current_solider_location):
+                    print("boom")
+
+
+
+main()
+
+
